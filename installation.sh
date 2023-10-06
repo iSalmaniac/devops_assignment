ab0007dac0f267591dcb050982460a44789ce8ec
iSalmaniac <salmaniacprashant@gmail.com>
2023-10-07 00:21:10 +0530

    updated

--- installation.sh
+++ installation.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
 
-sudo apt update 
-sudo apt install git -y 
+echo " ###################### PACKAGE  INSTALLATIONS ################### "
+echo ""
+echo ""
+sudo apt update
+echo ""
+echo ""
+echo ""
 sudo apt install nodejs -y
+echo ""
+echo ""
 sudo apt install npm -y
+echo ""
+echo ""
 sudo apt install pm2 -g
+echo ""
+echo ""
 sudo apt install nginx -y
-
-
-
+echo ""
+echo ""
+sudo apt clean
+echo ""
+echo ""
+echo ""
+echo "################### PACKAGE VERSIONS  ####################" 
+echo ""
+echo ""
+echo ""
+node -v
+echo ""
+npm -v
+echo""
+pm2 -v
+echo""
+nginx -v
+echo""
+echo ""
+echo ""
+echo " ###################### PACKAGE RUNNING STATUS ####################"
+echo ""
+sudo systemctl status nginx
+echo ""
+echo ""
+echo "################################### SCRIPT RUN COMPLETE #################################"
+echo ""
+echo ""
 exit 0
--- script.sh
+++ script.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 git clone https://github.com/iSalmaniac/devops_assignment.git
+
 cd /home/Ubuntu/devops_assignment
+
 sudo su
-npm install
-npm install pm2 -g
-pm2 start index.js --name devops_assignment --watch
-pm2 list
+
+sudo npm install
+
+sudo npm install pm2 -g
+
+sudo pm2 start index.js --name devops_assignment --watch
+
+sudo pm2 save
+
+sudo pm2 list
 
 
 exit 0
