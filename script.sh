db5405cba5895481c4c38f8f2e05fa508211509e
Salmaniac Prashant <96828332+iSalmaniac@users.noreply.github.com>
2023-10-07 12:21:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,15 +3,13 @@
 
 sudo npm install
 
-npm install pm2 -g
+npm install pm2@latest -g
 
 pm2 start index.js --name devops_assignment --watch
 
-pm2 unstartup systemd
+pm2 startup systemd
 
 pm2 save
-
-pm2 list
 
 
 exit 0
